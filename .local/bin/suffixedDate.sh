deb637c31b674843e3e16834de2429944593fbab
Medlei <medlei_@outlook.com>
2026-08-20 15:47:52 +0000

    add command checks

--- .local/bin/suffixedDate.sh
+++ .local/bin/suffixedDate.sh
@@ -1,12 +1,14 @@
 #/bin/sh
 
-date=`date +%d`
+command -v date &>/dev/null && {
+	date=`date +%d`
 
-case $(($date % 10)) in
-	*) dateSuffix=th;;
-	1) dateSuffix=st;;
-	2) dateSuffix=nd;;
-	3) dateSuffix=rd
-esac
+	command -v expr &>/dev/null && case `expr $date % 10` in
+		*) dateSuffix=th;;
+		1) dateSuffix=st;;
+		2) dateSuffix=nd;;
+		3) dateSuffix=rd
+	esac
 
-echo $date$dateSuffix
+	echo $date$dateSuffix
+}
