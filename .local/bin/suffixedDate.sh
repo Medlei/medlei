d9624a6f6c8f73e56325e7593e0263ea0e6e430d
Medlei <medlei_@outlook.com>
2026-08-22 01:29:16 +0000

    remove command checks

--- .local/bin/suffixedDate.sh
+++ .local/bin/suffixedDate.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-command -v date &>/dev/null && {
-	date=`date +%d`
+date=`date +%d`
 
-	command -v expr &>/dev/null && case `expr $date % 10` in
-		*) dateSuffix=th;;
-		1) dateSuffix=st;;
-		2) dateSuffix=nd;;
-		3) dateSuffix=rd
-	esac
+case `expr $date % 10` in
+	*) dateSuffix=th;;
+	1) dateSuffix=st;;
+	2) dateSuffix=nd;;
+	3) dateSuffix=rd
+esac
 
-	echo $date$dateSuffix
-}
+echo $date$dateSuffix
